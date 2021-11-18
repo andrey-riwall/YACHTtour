171de7373ea31f23269505da2147791a8411e866
riwall <83021906+andrey-riwall@users.noreply.github.com>
2021-11-19 01:05:42 +0300

    Delete app directory

--- app/js/main.js
+++
@@ -1,2 +0,0 @@
-const headerBtn=document.querySelectorAll(".nav__link");for(let e=0;e<headerBtn.length;e++)headerBtn[e].onclick=function(){let n=0;for(;n<headerBtn.length;)headerBtn[n++].className="nav__link";headerBtn[e].className="nav__link is_active"};AOS.init();
-//# sourceMappingURL=main.js.map
